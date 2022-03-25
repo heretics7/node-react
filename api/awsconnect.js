4c2fcf6476270a7523e813ae13d798fabdc2949f
heretics7 <heretics7@naver.com>
2022-03-25 17:04:26 +0900

    AWS RDS 연동 성공

---
+++ api/awsconnect.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var awsconnect = express.Router();
+var mysql = require('mysql');
+var dbconfig = require('../db/config');
+var connection = mysql.createConnection(dbconfig);
+
+awsconnect.get('/', (req, res) => {
+    connection.query('SELECT * FROM contact' , (error, result) => {
+        if(error) throw error;
+        console.log('DB 내용 :', result);
+        res.send(result);
+    })
+})
+
+module.exports = awsconnect;
--- server.js
+++ server.js
@@ -2,9 +2,11 @@ var express = require('express');
 var app = express();
 var getrouter = require('./api/getrouter');
 var postrouter = require('./api/postrouter');
+var awsconnect = require('./api/awsconnect');
 
 app.use('/get', getrouter);
 app.use('/post', postrouter);
+app.use('/awsconnect', awsconnect);
 
 app.set('port', 8080)
 app.get('/', (req, res) => {
